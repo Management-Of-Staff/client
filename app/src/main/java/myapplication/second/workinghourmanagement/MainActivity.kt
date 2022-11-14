0bf2dd665dd47c5a8f68c0e181d9b1dfd98c3557
jiyun <jiyun_99@naver.com>
2022-11-14 23:14:39 +0900

    login layout

--- app/src/main/java/myapplication/second/workinghourmanagement/MainActivity.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/MainActivity.kt
@@ -1,21 +1,19 @@
 package myapplication.second.workinghourmanagement
 
-import android.content.Intent
-import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import myapplication.second.workinghourmanagement.databinding.CommonLoginActivityBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: CommonLoginActivityBinding
+//    private lateinit var binding: CommonLoginActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.common_login_activity)
+        setContentView(R.layout.common_login_activity)
+//        binding = DataBindingUtil.setContentView(this, R.layout.common_login_activity)
 
-        binding.textJoin.setOnClickListener{
-            val intent = Intent(this, JoinActivity::class.java)
-            startActivity(intent)
-        }
+//        binding.textJoin.setOnClickListener{
+//            val intent = Intent(this, JoinActivity::class.java)
+//            startActivity(intent)
+//        }
 
     }
 }
