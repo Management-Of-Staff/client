5417a24229fa60d3234ddf90db37c361546ef805
jiyun <jiyun_99@naver.com>
2022-11-17 14:52:02 +0900

    회원가입 관련 activity 추가

---
+++ app/src/main/java/myapplication/second/workinghourmanagement/OwnerJoinActivity.kt
@@ -0,0 +1,29 @@
+package myapplication.second.workinghourmanagement
+
+import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+
+class OwnerJoinActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.owner_join_activity)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar: ActionBar = supportActionBar!!
+        actionBar.setDisplayShowTitleEnabled(false)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
