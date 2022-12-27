20485db9b43acf411ef1b95dded4cd9e21d4cec1
jiyun <jiyun_99@naver.com>
2022-12-28 01:53:31 +0900

    약관동의 레이아웃&액티비티

---
+++ app/src/main/java/myapplication/second/workinghourmanagement/OwnerTosActivity.kt
@@ -0,0 +1,38 @@
+package myapplication.second.workinghourmanagement
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import myapplication.second.workinghourmanagement.databinding.OwnerTosActivityBinding
+
+class OwnerTosActivity : AppCompatActivity() {
+    private lateinit var binding: OwnerTosActivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.owner_tos_activity)
+
+        binding.buttonNext.setOnClickListener {
+            val intent = Intent(this, OwnerJoinActivity::class.java)
+            startActivity(intent)
+        }
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar: ActionBar = supportActionBar!!
+        actionBar.setDisplayShowTitleEnabled(false)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
