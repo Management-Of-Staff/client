35147ecf2597d7e1e75849804ae89f3876d0224a
jiyun <jiyun_99@naver.com>
2022-12-28 01:54:57 +0900

    전화번호인증 레이아웃&액티비티 추가

--- app/src/main/java/myapplication/second/workinghourmanagement/OwnerJoinActivity.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/OwnerJoinActivity.kt
@@ -1,15 +1,25 @@
 package myapplication.second.workinghourmanagement
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import myapplication.second.workinghourmanagement.databinding.OwnerJoinActivityBinding
 
-class OwnerJoinActivity: AppCompatActivity() {
+class OwnerJoinActivity : AppCompatActivity() {
+    private lateinit var binding: OwnerJoinActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.owner_join_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.owner_join_activity)
+
+        binding.ownerJoinBtnNext.setOnClickListener {
+            val intent = Intent(this, OwnerJoinInfoActivity::class.java)
+            startActivity(intent)
+        }
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -17,8 +27,9 @@ class OwnerJoinActivity: AppCompatActivity() {
         actionBar.setDisplayShowTitleEnabled(false)
         actionBar.setDisplayHomeAsUpEnabled(true)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             android.R.id.home -> {
                 finish()
                 return true
