5c7c9f8a3f0d4cea924c4089912da6f7f9cb84a5
thsamajiki <ant1102@naver.com>
2022-12-30 03:00:00 +0900

    (오너) 기본 주소 검색 목록 어댑터 1차 구현

---
+++ app/src/main/java/myapplication/second/workinghourmanagement/OwnerSearchBaseAddressAdapter.kt
@@ -0,0 +1,17 @@
+package myapplication.second.workinghourmanagement
+
+import androidx.recyclerview.widget.RecyclerView
+import myapplication.second.workinghourmanagement.databinding.ItemOwnerConvertStoreListBinding
+import myapplication.second.workinghourmanagement.databinding.ItemOwnerSearchBaseAddressListBinding
+
+class OwnerSearchBaseAddressAdapter {
+
+    class SearchBaseAddressViewHolder(
+        private val binding: ItemOwnerSearchBaseAddressListBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind() {
+
+        }
+    }
+}
