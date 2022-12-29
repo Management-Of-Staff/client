6ce5f3881192145dc36fca7bcd3336c03d48ca0c
jiyun <jiyun_99@naver.com>
2022-12-29 19:44:58 +0900

    비밀번호, 사업자등록번호 유효성 검사 추가

--- app/src/main/java/myapplication/second/workinghourmanagement/OwnerJoinInfoActivity.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/OwnerJoinInfoActivity.kt
@@ -2,6 +2,9 @@ package myapplication.second.workinghourmanagement
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +18,24 @@ class OwnerJoinInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.owner_join_info_activity)
 
+        bind()
+
+        setSupportActionBar(binding.toolbar)
+        val actionBar: ActionBar = supportActionBar!!
+        actionBar.setDisplayShowTitleEnabled(false)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun bind() {
+        binding.ownerJoinEditBusinessNum.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                val bn = binding.ownerJoinEditBusinessNum.text.toString()
+                binding.ownerJoinBtnBnCheck.isEnabled = checkBusinessNumValidation(bn)
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
         binding.ownerJoinBtnJoin.setOnClickListener {
             //todo 회원가입 성공여부 체크후 성공시 intent
             val intent = Intent(this, OwnerJoinCompleteActivity::class.java)
@@ -22,10 +43,36 @@ class OwnerJoinInfoActivity : AppCompatActivity() {
             finishAffinity()
         }
 
-        setSupportActionBar(binding.toolbar)
-        val actionBar: ActionBar = supportActionBar!!
-        actionBar.setDisplayShowTitleEnabled(false)
-        actionBar.setDisplayHomeAsUpEnabled(true)
+        binding.ownerJoinEditPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val password = binding.ownerJoinEditPassword.text.toString()
+                if (checkPasswordValidation(password)) {
+                    binding.passwordValidator.setTextColor(resources.getColor(R.color.success1))
+                } else {
+                    binding.passwordValidator.setTextColor(resources.getColor(R.color.error1))
+                }
+            }
+        })
+
+        binding.ownerJoinEditPasswordConfirm.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (binding.ownerJoinEditPassword.text.toString() != binding.ownerJoinEditPasswordConfirm.text.toString()) {
+                    binding.passwordConfirmValidator.setText(R.string.password_disMatch)
+                    binding.passwordConfirmValidator.setTextColor(resources.getColor(R.color.error1))
+                    binding.ownerJoinBtnJoin.isEnabled = false
+                } else {
+                    binding.passwordConfirmValidator.setText(R.string.password_match)
+                    binding.passwordConfirmValidator.setTextColor(resources.getColor(R.color.success1))
+
+                    //todo 모든 정보 입력했는지 확인한 후 enabled = true 설정!
+                    binding.ownerJoinBtnJoin.isEnabled = true
+                }
+            }
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -36,5 +83,23 @@ class OwnerJoinInfoActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun checkPasswordValidation(pw: String): Boolean {
+        return pw.matches("^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$".toRegex())
+    }
+
+    private fun checkBusinessNumValidation(bn: String): Boolean {
+        return if (bn.length == 10) {
+            val keyArr = arrayOf(1, 3, 7, 1, 3, 7, 1, 3, 5)
+            var check = 0
+
+            keyArr.forEachIndexed { index, i ->
+                check += i * bn[index].digitToInt()
+            }
+            check += (bn[8].digitToInt() * keyArr[8]) / 10
+
+            bn[9].digitToInt() == (10 - (check % 10))
+        } else false
     }
 }
