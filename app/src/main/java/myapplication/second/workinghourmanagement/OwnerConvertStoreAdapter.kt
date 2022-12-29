0ad22997b486754e4bdf7d07fba23e2fc3e2ef1d
thsamajiki <ant1102@naver.com>
2022-12-29 22:18:45 +0900

    OwnerConvertStoreAdapter.kt 기본 세팅

---
+++ app/src/main/java/myapplication/second/workinghourmanagement/OwnerConvertStoreAdapter.kt
@@ -0,0 +1,16 @@
+package myapplication.second.workinghourmanagement
+
+import androidx.recyclerview.widget.RecyclerView
+import myapplication.second.workinghourmanagement.databinding.ItemOwnerConvertStoreListBinding
+
+class OwnerConvertStoreAdapter {
+
+    class ConvertStoreViewHolder(
+        private val binding: ItemOwnerConvertStoreListBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind() {
+
+        }
+    }
+}
