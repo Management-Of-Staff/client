94bf41acf9e22df4128fd84cf6b4fed07234714f
jiyun <jiyun_99@naver.com>
2023-01-08 15:57:55 +0900

    회원가입 api 연동(회원가입 성공했을 경우만) - 회원가입 실패는 api 개발 완료 후 진행할 예정

--- app/src/main/java/myapplication/second/workinghourmanagement/member/OwnerJoinInfoActivity.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/member/OwnerJoinInfoActivity.kt
@@ -1,23 +1,33 @@
 package myapplication.second.workinghourmanagement.member
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import myapplication.second.workinghourmanagement.R
+import myapplication.second.workinghourmanagement.*
 import myapplication.second.workinghourmanagement.databinding.ActivityOwnerJoinInfoBinding
+import myapplication.second.workinghourmanagement.dto.ResultResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class OwnerJoinInfoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOwnerJoinInfoBinding
+    private lateinit var service: RetrofitService
+    private lateinit var customDialog: CustomDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_owner_join_info)
 
+        service = RetrofitManager.retrofit.create(RetrofitService::class.java)
         bind()
 
         setSupportActionBar(binding.toolbar)
@@ -36,11 +46,15 @@ class OwnerJoinInfoActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
+        binding.ownerJoinBtnBnCheck.setOnClickListener {
+            customDialog = CustomDialog(this, getString(R.string.bn_success))
+            customDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            customDialog.show()
+            customDialog.shutdownClick.setOnClickListener { customDialog.dismiss() }
+        }
+
         binding.ownerJoinBtnJoin.setOnClickListener {
-            //todo 회원가입 성공여부 체크후 성공시 intent
-            val intent = Intent(this, OwnerJoinCompleteActivity::class.java)
-            startActivity(intent)
-            finishAffinity()
+            registerUser()
         }
 
         binding.ownerJoinEditPassword.addTextChangedListener(object : TextWatcher {
@@ -73,6 +87,63 @@ class OwnerJoinInfoActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun joinSuccess() {
+        val intent = Intent(this, OwnerJoinCompleteActivity::class.java)
+        startActivity(intent)
+        finishAffinity()
+    }
+
+    private fun registerUser() {
+        val myIntent = intent
+        val phone = myIntent.getStringExtra("phone")!!
+        val uuid = myIntent.getStringExtra("uuid")!!
+//        val user = User(
+//            uuid,
+//            binding.ownerJoinEditOwnerName.text.toString(),
+//            "01000000003",
+//            binding.ownerJoinEditPassword.text.toString(),
+//            "OWNER"
+//        )
+        val userInfo = HashMap<String, String>()
+//        userInfo["uuid"] = uuid
+        userInfo["name"] = binding.ownerJoinEditOwnerName.text.toString()
+        userInfo["phone"] = "01000000004"
+        //todo 유저 입력으로 변경하기(지금은 테스트용)
+//        userInfo["phone"] = phone
+        userInfo["password"] = binding.ownerJoinEditPassword.text.toString()
+        userInfo["role"] = "OWNER"
+
+//        service.registerOwner(user.name, user.phone, user.password, user.role)
+        service.registerOwner(userInfo)
+            .enqueue(object : Callback<ResultResponse> {
+                override fun onResponse(
+                    call: Call<ResultResponse>,
+                    response: Response<ResultResponse>
+                ) {
+                    if (response.isSuccessful.not()) {
+//                        Log.d("보내긴함 근데 실패,,,", "${response.body()}")
+//                        Log.d("response는 뭐가 나와?", "${response.raw()}")
+                        return
+                    }
+                    val body = response.body()
+                    if (body != null) {
+                        Log.d("data", body.data.toString())
+                        Log.d("statusCode", body.statusCode.toString())
+                        Log.d("message", body.message)
+
+                        if (body.statusCode == 200) joinSuccess()
+                    }
+//                    response.body()?.let {
+//                        Log.d("getPosts success", "\n$it")
+//                    }
+                }
+
+                override fun onFailure(call: Call<ResultResponse>, t: Throwable) {
+                    Log.d("getPosts fail", "[Fail]$t")
+                }
+            })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
