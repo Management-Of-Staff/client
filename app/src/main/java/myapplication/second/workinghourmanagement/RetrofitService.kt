65895bd91011ee02cdf0acd59a0d1a78ce2ed8f0
jiyun <jiyun_99@naver.com>
2023-01-08 15:55:45 +0900

    Retrofit 인터페이스 변경(오너 회원가입, 로그인)

--- app/src/main/java/myapplication/second/workinghourmanagement/RetrofitService.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/RetrofitService.kt
@@ -1,23 +1,37 @@
 package myapplication.second.workinghourmanagement
 
+import myapplication.second.workinghourmanagement.dto.ResultLogin
+import myapplication.second.workinghourmanagement.dto.ResultResponse
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
-
+import retrofit2.http.*
 
 interface RetrofitService {
-
-    //    @FormUrlEncoded
-//    @POST("/owners/create")
-//    fun joinOwner(
-//        @Field("userInfo") userInfo: Join,
-//        @Field("UUID") source: String,
-//        @Field("name") target: String,
+    // Owner
+//    @FormUrlEncoded
+    @Headers("Content-Type: application/json")
+    @POST("owners/register")
+    fun registerOwner(
+//        @Field("uuid") uuid: String,
+//        @Field("name") name: String,
 //        @Field("phone") phone: String,
 //        @Field("password") password: String,
 //        @Field("role") role: String,
-//        ): Call<String>
-    @GET("/posts/{post}")
+        @Body params: HashMap<String, String>
+    ): Call<ResultResponse>
+
+    @POST("auth/login")
+    fun loginOwner(
+        @Body params: HashMap<String, String>,
+    ): Call<ResultLogin>
+
+    @GET("owners/{token}")
+    fun getOwner(
+        @Path("token") token: String?
+    ): Call<PostResult>
+
+    // Staff
+
+    @GET("posts/{post}")
     fun getPosts(
         @Path("post") post: String?
     ): Call<PostResult>
