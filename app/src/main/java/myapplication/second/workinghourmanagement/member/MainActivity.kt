62b157980da8dfce7a8cdc06e6072538a3ca3df5
jiyun <jiyun_99@naver.com>
2023-01-09 21:08:19 +0900

    로그인 실패시 실패메시지 다이얼로그 띄우기

--- app/src/main/java/myapplication/second/workinghourmanagement/member/MainActivity.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/member/MainActivity.kt
@@ -1,10 +1,13 @@
 package myapplication.second.workinghourmanagement.member
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import myapplication.second.workinghourmanagement.CustomDialog
 import myapplication.second.workinghourmanagement.R
 import myapplication.second.workinghourmanagement.RetrofitManager
 import myapplication.second.workinghourmanagement.RetrofitService
@@ -16,18 +19,23 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCommonLoginBinding
-    lateinit var service: RetrofitService
+    private lateinit var service: RetrofitService
+    private lateinit var customDialog: CustomDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_common_login)
 
         service = RetrofitManager.retrofit.create(RetrofitService::class.java)
+        bind()
+    }
 
+    private fun bind() {
         binding.textJoin.setOnClickListener {
             val intent = Intent(this, OwnerTosActivity::class.java)
             startActivity(intent)
         }
+
         binding.textFindPassword.setOnClickListener {
             val intent = Intent(this, FindPasswdActivity::class.java)
             startActivity(intent)
@@ -46,13 +54,16 @@ class MainActivity : AppCompatActivity() {
         service.loginOwner(loginRequestParams).enqueue(object : Callback<ResultLogin> {
             override fun onResponse(call: Call<ResultLogin>, response: Response<ResultLogin>) {
                 if (response.isSuccessful.not()) {
-//                    Log.d("ㄱ half success", "${response.raw()}")
                     return
                 }
                 val body = response.body()
                 if (body != null) {
-                    Log.d("accessToken", body.accessToken)
-                    Log.d("refreshToken", body.refreshToken)
+                    if (!body.accessToken.isNullOrBlank() && !body.refreshToken.isNullOrBlank()) {
+                        //todo 토큰 저장 후 intent
+                    } else {
+                        Log.d("response.message", body.message!!)
+                        errorMessageDialog(body.message)
+                    }
                 }
             }
 
@@ -60,5 +71,12 @@ class MainActivity : AppCompatActivity() {
                 Log.d("getPosts fail", "[Fail]$t")
             }
         })
+    }
+
+    fun errorMessageDialog(message: String) {
+        customDialog = CustomDialog(this, message)
+        customDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        customDialog.show()
+        customDialog.shutdownClick.setOnClickListener { customDialog.dismiss() }
     }
 }
