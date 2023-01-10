03bb49d18ce6f8ad6259f6abe57356ed8e34b9bc
jiyun <jiyun_99@naver.com>
2023-01-11 01:50:53 +0900

    checkBNum() : [POST]사업자등록번호조회

--- app/src/main/java/myapplication/second/workinghourmanagement/RetrofitService.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/RetrofitService.kt
@@ -1,5 +1,6 @@
 package myapplication.second.workinghourmanagement
 
+import myapplication.second.workinghourmanagement.dto.ResultBnumCheck
 import myapplication.second.workinghourmanagement.dto.ResultLogin
 import myapplication.second.workinghourmanagement.dto.ResultResponse
 import retrofit2.Call
@@ -7,7 +8,14 @@ import retrofit2.http.*
 
 interface RetrofitService {
     // Owner
-//    @FormUrlEncoded
+    @Headers("Content-Type: application/json")
+    @POST("status")
+    fun checkBNum(
+        @Query("serviceKey") serviceKey: String,
+        @Body params: HashMap<String, List<String>>
+    ): Call<ResultBnumCheck>
+
+    //    @FormUrlEncoded
     @Headers("Content-Type: application/json")
     @POST("owners/register")
     fun registerOwner(
