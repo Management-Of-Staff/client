57d4ce2c6d47e1276fcdf5727263678dbd36d326
jiyun <jiyun_99@naver.com>
2023-02-02 02:05:08 +0900

    interceptor 추가

--- app/src/main/java/myapplication/second/workinghourmanagement/RetrofitManager.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/RetrofitManager.kt
@@ -1,16 +1,20 @@
 package myapplication.second.workinghourmanagement
 
-import myapplication.second.workinghourmanagement.BuildConfig.BASE_URL
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitManager {
-    private val BASE_URL: String = BuildConfig.BASE_URL
-
     companion object{
+        private val BASE_URL: String = BuildConfig.BASE_URL
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor())
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
-//            .baseUrl("https://jsonplaceholder.typicode.com")  //test용
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
