f5f2c941d72155763710346c0866bfbd2dd58f7e
jiyun <jiyun_99@naver.com>
2023-02-13 15:39:57 +0900

    403에러 발생시 토큰재발급 #49

--- app/src/main/java/myapplication/second/workinghourmanagement/AuthInterceptor.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/AuthInterceptor.kt
@@ -1,9 +1,13 @@
 package myapplication.second.workinghourmanagement
 
 import android.util.Log
+import kotlinx.coroutines.runBlocking
+import myapplication.second.workinghourmanagement.dto.ResultToken
 import okhttp3.Interceptor
 import okhttp3.Response
 import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
 
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
@@ -13,10 +17,52 @@ class AuthInterceptor : Interceptor {
             .build()
         val response = chain.proceed(request)
 
-        val responseJson = response.extractResponseJson()
-        val bodyCode = responseJson.getString("status")
-        Log.d("bodyCode 출력", bodyCode)
+        when (response.code) {
+            403 -> {
+                val responseJson = response.extractResponseJson()
+                val bodyCode = responseJson.getString("status")
+                // 411 : accessToken 만료
+                // todo 서버 배포되면 에러코드 수정하기(403 -> 411)
+                if (bodyCode == "403") {
+                    // todo 토큰재발급 후 저장(동기처리)
 
+                    val refreshToken = "Bearer " + MyApplication.prefs.getString("refreshToken")
+                    RetrofitManager.loginRetrofit.create(RetrofitService::class.java)
+                        .reissue(refreshToken).enqueue(object : Callback<ResultToken> {
+                            override fun onResponse(
+                                call: Call<ResultToken>,
+                                response: retrofit2.Response<ResultToken>
+                            ) {
+                                MyApplication.prefs.setString(
+                                    "accessToken",
+                                    response.body()!!.accessToken.toString()
+                                )
+                                MyApplication.prefs.setString(
+                                    "refreshToken",
+                                    response.body()!!.refreshToken.toString()
+                                )
+                            }
+
+                            override fun onFailure(call: Call<ResultToken>, t: Throwable) {
+                                Log.e("changPassword fail", "[Fail]$t")
+                            }
+                        })
+
+                    // todo 동기처리하기 앞에서 저장이 완료된 후에 request 날리도록!
+                    val newAccessToken = MyApplication.prefs.getString("accessToken")
+                    Log.d("newAccessToken", newAccessToken)
+                    val newRequest = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer $newAccessToken")
+                        .build()
+                    return chain.proceed(newRequest)
+                }
+                // 412 : refreshToken 만료 -> 토큰 초기화하고, 로그인 페이지로 이동
+                else if (bodyCode == "412") {
+                    MyApplication.prefs.setString("accessToken", "")
+                    MyApplication.prefs.setString("refreshToken", "")
+                }
+            }
+        }
         return response
     }
 
