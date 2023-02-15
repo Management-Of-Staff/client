5d9369c8bcce71aa5e635abeffe1da2b97dba1a6
jiyun <jiyun_99@naver.com>
2023-02-15 19:58:24 +0900

    renamed from activity_owner_invite_staff.xml

--- app/src/main/java/myapplication/second/workinghourmanagement/manageStaff/InviteStaffActivity.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/manageStaff/InviteStaffActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import myapplication.second.workinghourmanagement.R
-import myapplication.second.workinghourmanagement.databinding.ActivityOwnerInviteStaffBinding
+import myapplication.second.workinghourmanagement.databinding.ActivityInviteStaffBinding
 
 class InviteStaffActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityOwnerInviteStaffBinding
+    private lateinit var binding: ActivityInviteStaffBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_owner_invite_staff)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_invite_staff)
     }
 }
