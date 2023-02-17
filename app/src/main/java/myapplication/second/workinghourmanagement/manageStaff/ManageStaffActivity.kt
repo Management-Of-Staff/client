890d8255104b6a69fcc21da061827e1da45e9962
jiyun <jiyun_99@naver.com>
2023-02-17 17:54:36 +0900

    MCLM-145 직급 선택 spinner

--- app/src/main/java/myapplication/second/workinghourmanagement/manageStaff/ManageStaffActivity.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/manageStaff/ManageStaffActivity.kt
@@ -2,6 +2,7 @@ package myapplication.second.workinghourmanagement.manageStaff
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import myapplication.second.workinghourmanagement.R
@@ -23,6 +24,14 @@ class ManageStaffActivity : AppCompatActivity() {
             val bottomSheet = BottomSheetAddWorkSchedule(this)
             bottomSheet.show(supportFragmentManager, bottomSheet.tag)
         }
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.position,
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerStaffPosition.adapter = adapter
 
         binding.btnViewStaffProfile.setOnClickListener {
             val intent = Intent(this, StaffPersonalInfoActivity::class.java)
