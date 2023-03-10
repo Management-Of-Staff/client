226da9946525d12715c179cb89d48b4b9b3fcfa5
jae1jeong <wodnjs3600@naver.com>
2023-03-10 19:39:28 +0900

    feat/hilt network module 추가

--- app/src/main/java/myapplication/second/workinghourmanagement/module/NetworkModule.kt
+++ app/src/main/java/myapplication/second/workinghourmanagement/module/NetworkModule.kt
@@ -1,4 +1,52 @@
 package myapplication.second.workinghourmanagement.module
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import myapplication.second.workinghourmanagement.AuthInterceptor
+import myapplication.second.workinghourmanagement.BuildConfig
+import myapplication.second.workinghourmanagement.RetrofitService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    @Provides
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
+         val client = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+
+        if(BuildConfig.DEBUG){
+            client.addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+        }
+        return client.build()
+    }
+
+    @Provides
+    fun provideAuthInterceptor(): AuthInterceptor {
+        return AuthInterceptor()
+    }
+
+    @Provides
+    fun provideRetrofitService(retrofit: Retrofit): RetrofitService {
+        return retrofit.create(RetrofitService::class.java)
+    }
 }
